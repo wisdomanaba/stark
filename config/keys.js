a300eb1df35f4a7d9b4993030dd5058a85ab6f0a
wizzydev <wisdomanaba83@gmail.com>
2020-07-25 13:41:33 +0100

    Updated keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
 module.exports = {
-      // mongoURI: 'mongodb://localhost:27017/stark'
-      mongoURI: 'mongodb+srv://wisdom:developer+Wian+2001@cluster0-w1tf1.mongodb.net/Cluster0?retryWrites=true&w=majority'
-      
+      mongoURI: process.env.MONGOURI
 }
